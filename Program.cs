6ccdeab02d034369226885fa51ec445c97a62940
Bhagi <puppyreddy1997@gmail.com>
2020-11-22 22:35:57 +0530

    [Bhagyalaxmi]ADDED:Added code to Check The lengths or equal or not

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace LineComparison
         static void Main(string[] args)
         {
             Console.WriteLine("WelCome To Line Comparison Computation Program");
-            int x1, x2, y1, y2;
+            int x1, x2,x3,x4, y1, y2,y3,y4;
 
             Console.WriteLine("Enter the Values of x1 and y1 Co-ordinates");
             x1 = Convert.ToInt32(Console.ReadLine());
@@ -17,8 +17,28 @@ namespace LineComparison
             x2 = Convert.ToInt32(Console.ReadLine());
             y2 = Convert.ToInt32(Console.ReadLine());
 
-            double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-            Console.WriteLine("Length of line is : " + length);
+            double line_One_Length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("Length of line is : " + line_One_Length);
+
+            Console.WriteLine("Enter the Values of x3 and y3 Co-ordinates");
+            x3 = Convert.ToInt32(Console.ReadLine());
+            y3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Values of x4 and y4 Co-ordinates");
+            x4 = Convert.ToInt32(Console.ReadLine());
+            y4 = Convert.ToInt32(Console.ReadLine());
+
+            double line_Two_Length = Math.Sqrt(Math.Pow(x4 - x3, 2) + Math.Pow(y4 - y3, 2));
+            Console.WriteLine("Length of line is : " + line_Two_Length);
+
+            if (line_One_Length.Equals(line_Two_Length))
+            {
+                Console.WriteLine("Length of Two Lines are Equal");
+            }
+            else
+            {
+                Console.WriteLine("Length of Two Lines are Not Equal");
+            }
         }
     }
 }
