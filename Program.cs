7d08cf479dbf41b9ee5042be45175ad3bb3339ac
Bhagi <puppyreddy1997@gmail.com>
2020-11-22 22:18:44 +0530

    [Bhagyalaxmi]ADDED:Added code to Calculate the Length Of Line

--- Program.cs
+++ Program.cs
@@ -7,6 +7,18 @@ namespace LineComparison
         static void Main(string[] args)
         {
             Console.WriteLine("WelCome To Line Comparison Computation Program");
+            int x1, x2, y1, y2;
+
+            Console.WriteLine("Enter the Values of x1 and y1 Co-ordinates");
+            x1 = Convert.ToInt32(Console.ReadLine());
+            y1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Values of x2 and y2 Co-ordinates");
+            x2 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
+
+            double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("Length of line is : " + length);
         }
     }
 }
